a9028de67413e76c2e470867b87145e9e4c16572
Ali Akturin <ali.akturin2@gmail.com>
2022-01-30 22:57:46 -0800

    Initial Commit

---
+++ memorize/memorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorizeApp.swift
+//  memorize
+//
+//  Created by Ali Akturin on 1/30/22.
+//
+
+import SwiftUI
+
+@main
+struct memorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
